627fd60c4011986458fcc940da6322b468bad15e
Malcolm Harrow <malcolm.harrow@gmail.com>
2016-10-08 21:47:45 +0800

    correct misnamed file

--- module4/src/menuapp/categories.component.js
+++ module4/src/menuapp/categories.component.js
@@ -3,7 +3,7 @@
 
 angular.module('MenuApp')
 .component('categories', {
-  templateUrl: 'src/menuapp/templates/categorycomponent.template.html',
+  templateUrl: 'src/menuapp/templates/categoriescomponent.template.html',
   bindings: {
     items: '<'
   }
